680554e7999041e3fea221084f8e1b6cfdba4773
JayBigGuy10 <jayden.pl@Outlook.com>
2026-08-11 14:57:47 +1200

    update meta cause koreader logs says fullname is deprecated

--- _meta.lua
+++ _meta.lua
@@ -1,6 +1,5 @@
 local _ = require("gettext")
 return {
-    name = "AO3Downloader",
     fullname = _("AO3 downloader"),
     description = _([[Browse for and download fanfiction from Archive Of Our Own]]),
 }
